0011d5e500b1a366435fa72e8ba39f01148c9225
Yotam Manor <yotammanor@gmail.com>
2017-08-14 15:21:00 +0300

    import factories to model __init__, move test from accelerate.

--- accelerator/tests/test_currency.py
+++ accelerator/tests/test_currency.py
@@ -7,8 +7,14 @@ from .factories.currency_factory import CurrencyFactory
 
 
 class TestCurrency(TestCase):
-
     def test_currency(self):
         CurrencyFactory(name="US Dollars",
                         abbr="USD")
         assert Currency.objects.get(abbr="USD")
+
+    def test_usd_default(self):
+        CurrencyFactory(abbr="GBP")
+        usd_name = "US Dollars"
+        cur_usd = CurrencyFactory(abbr="USD", name=usd_name)
+        assert cur_usd == Currency.default_currency()
+        assert usd_name in str(cur_usd)
