90fab7f0f6204c553f9483a6aed798c7fa29fc66
Nathan Wilson <nathan@masschallenge.org>
2017-08-31 20:39:14 +0000

    Coverage for remaining models

---
+++ accelerator/tests/test_job_posting.py
@@ -0,0 +1,12 @@
+# MIT License
+# Copyright (c) 2017 MassChallenge, Inc.
+
+from django.test import TestCase
+from .factories.job_posting_factory import JobPostingFactory
+
+
+class TestJobPosting(TestCase):
+    def test_job_posting(self):
+        posting = JobPostingFactory()
+        self.assertTrue(posting.title in str(posting))
+        self.assertTrue(posting.startup.organization.name in str(posting))
