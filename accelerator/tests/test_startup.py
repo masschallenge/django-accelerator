9240344fd70d69a71dddb39b6fd4c852b6254941
Nathan Wilson <nathan@masschallenge.org>
2017-09-06 15:07:03 +0000

    Removed old comment

--- accelerator/tests/test_startup.py
+++ accelerator/tests/test_startup.py
@@ -6,14 +6,6 @@ from .factories.startup_factory import StartupFactory
 from unittest import skip
 
 
-# For some reason running StartupFactory() triggers an
-# error that reports:
-#
-#   django.db.utils.OperationalError: no such table: auth_user
-#
-# AC-4806 is a bug ticket to fix this.
-
-# @skip("StartupFactory generates an error.  See AC-4806")
 class TestStartup(TestCase):
     def test_startup(self):
         startup = StartupFactory()
