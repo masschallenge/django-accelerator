c4a382523fe76d46d95dd26ffefa497a976e9dba
Nathan Wilson <nathan@masschallenge.org>
2017-11-03 17:23:29 +0000

    Move EmailModelBackend to simpleuser

---
+++ simpleuser/email_model_backend.py
@@ -0,0 +1,20 @@
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class EmailModelBackend(object):
+
+    def authenticate(self, email=None, password=None):
+        try:
+            user = User.objects.get(email=email)
+            if user.check_password(password):
+                return user
+        except User.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
