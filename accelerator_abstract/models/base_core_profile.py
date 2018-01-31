651cd82d8c51722cac141840542c59430bbcdf66
Yotam Manor <yotammanor@gmail.com>
2018-01-31 17:08:59 +0200

    add missing function BaseCoreProfile.full_name

--- accelerator_abstract/models/base_core_profile.py
+++ accelerator_abstract/models/base_core_profile.py
@@ -116,3 +116,12 @@ class BaseCoreProfile(AcceleratorModel):
         if self.user_type is not None:
             ptype = ("%s " % self.user_type).title()
         return "%sProfile for %s" % (ptype, identifier)
+
+    def full_name(self):
+        fn = self.user.first_name
+        ln = self.user.last_name
+        if fn and ln:
+            name = u"%s %s" % (fn, ln)
+        else:
+            name = str(self.user.username)
+        return name
