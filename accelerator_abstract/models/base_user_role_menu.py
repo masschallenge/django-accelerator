ec51151ed4406edcbd880e2bfb71abe3b90747d9
Yotam Manor <yotammanor@gmail.com>
2018-02-04 14:31:54 +0200

    remove unused decorator from BaseUserRoleMenu

--- accelerator_abstract/models/base_user_role_menu.py
+++ accelerator_abstract/models/base_user_role_menu.py
@@ -5,14 +5,12 @@ from __future__ import unicode_literals
 
 import swapper
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 from fluent_pages.models import Page
 
 from accelerator_abstract.models.accelerator_model import AcceleratorModel
 from accelerator_abstract.models.base_program import PROGRAM_STATUSES
 
 
-@python_2_unicode_compatible
 class BaseUserRoleMenu(Page, AcceleratorModel):
     program_family = models.ForeignKey(
         swapper.get_model_name(AcceleratorModel.Meta.app_label,
