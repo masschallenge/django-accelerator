f8ad1743b83a24d869d8458e7eba1b416fedfeef
Yotam Manor <yotammanor@gmail.com>
2018-02-07 19:54:19 +0200

    swappable application_type model in factory

--- accelerator/tests/factories/application_type_factory.py
+++ accelerator/tests/factories/application_type_factory.py
@@ -3,21 +3,24 @@
 
 from __future__ import unicode_literals
 
+import swapper
 from factory import (
     DjangoModelFactory,
     Sequence,
     SubFactory,
 )
 
-from accelerator.models.application_type import ApplicationType
+from accelerator.apps import AcceleratorConfig
 from accelerator.tests.factories.startup_label_factory import StartupLabelFactory
+
+ApplicationType = swapper.load_model(AcceleratorConfig.name, 'ApplicationType')
 
 
 class ApplicationTypeFactory(DjangoModelFactory):
     class Meta:
         model = ApplicationType
 
-    name = Sequence(lambda n: "Application Type {0}".format(n))
+    name = Sequence(lambda n: 'Application Type {0}'.format(n))
     description = Sequence(lambda n:
-                           "Application Type Description {0}".format(n))
+                           'Application Type Description {0}'.format(n))
     submission_label = SubFactory(StartupLabelFactory)
