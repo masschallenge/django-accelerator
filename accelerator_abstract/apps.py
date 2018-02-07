9f9251fbb202fe5b21f669b74dd61f1e533985b1
Yotam Manor <yotammanor@gmail.com>
2018-02-07 15:33:34 +0200

    add swappable `get_profile` method to User model

--- accelerator_abstract/apps.py
+++ accelerator_abstract/apps.py
@@ -1,8 +1,17 @@
 # MIT License
 # Copyright (c) 2017 MassChallenge, Inc.
-
+import swapper
 from django.apps import AppConfig
+from django.contrib.auth import get_user_model
+from accelerator.apps import AcceleratorConfig
 
 
-class AcceleratorConfig(AppConfig):
+class AcceleratorAbstractConfig(AppConfig):
     name = 'accelerator_abstract'
+
+    def ready(self):
+        User = get_user_model()
+        BaseProfile = swapper.load_model(AcceleratorConfig.name, 'BaseProfile')
+        User.add_to_class('get_profile',
+                          lambda user: BaseProfile.objects.get(
+                              email=user.email))
