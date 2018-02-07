b3570c1d221528299d93364d0759c008f433325b
Yotam Manor <yotammanor@gmail.com>
2018-02-07 14:08:53 +0200

    add a todo to startup related names

--- accelerator_abstract/models/base_startup.py
+++ accelerator_abstract/models/base_startup.py
@@ -30,7 +30,7 @@ class BaseStartup(AcceleratorModel):
         AcceleratorModel.Meta.app_label, 'Organization'), blank=True,
         null=True, related_name='startups')
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             related_name='acc_startups')
+                             related_name='acc_startups')  # todo: rename
     is_visible = models.BooleanField(
         default=True,
         help_text=('Startup Profiles will be published to external websites '
