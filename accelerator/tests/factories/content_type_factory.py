650abefa5f24550db7c88bef7e4ed6c715b23290
Yotam Manor <yotammanor@gmail.com>
2018-02-08 16:48:21 +0200

    remove deprecated field from content_type_factory

--- accelerator/tests/factories/content_type_factory.py
+++ accelerator/tests/factories/content_type_factory.py
@@ -14,6 +14,5 @@ class ContentTypeFactory(DjangoModelFactory):
     class Meta:
         model = ContentType
 
-    name = Sequence(lambda n: "test_contenttype{0}".format(n))
     app_label = "mc"
     model = Sequence(lambda n: "test_contenttypemodel{0}".format(n))
