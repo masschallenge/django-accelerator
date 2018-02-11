53f4ae25bf827aab8387fcf32587b4932fbef648
Yotam Manor <yotammanor@gmail.com>
2018-02-11 12:14:47 +0200

    fix broken ExpertCategoryFactory.

--- accelerator/tests/factories/expert_category_factory.py
+++ accelerator/tests/factories/expert_category_factory.py
@@ -1,18 +1,21 @@
 # MIT License
 # Copyright (c) 2017 MassChallenge, Inc.
+from __future__ import unicode_literals
 
+import swapper
 from factory import (
     DjangoModelFactory,
     Iterator,
 )
 
-from accelerator.models import (
-    ExpertCategory,
-    VALID_EXPERT_CATEGORIES,
-)
+from accelerator.apps import AcceleratorConfig
+from accelerator.models import VALID_EXPERT_CATEGORIES
+
 # The import of VALID_EXPERT_CATEGORIES is indicative of a deeper problem with
 # how ExpertCategorys are currently used.  See AC-5022 for the underlying
 # issue and a discussion of possible better long term solutions.
+
+ExpertCategory = swapper.load_model(AcceleratorConfig.name, 'ExpertCategory')
 
 
 class ExpertCategoryFactory(DjangoModelFactory):
