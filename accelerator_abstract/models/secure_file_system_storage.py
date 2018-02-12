82aa79ed968978074e865ddca084e1ae1830a4b7
Yotam Manor <yotammanor@gmail.com>
2018-02-12 13:53:33 +0200

    add backward compatibility coverage skip flag in secure_file_system_storage

--- accelerator_abstract/models/secure_file_system_storage.py
+++ accelerator_abstract/models/secure_file_system_storage.py
@@ -11,7 +11,7 @@ from django.conf import settings
 try:
     from django.urls import reverse
 except ImportError:
-    from django.core.urlresolvers import reverse
+    from django.core.urlresolvers import reverse  # pragma: no cover
 
 
 class SecureFileSystemStorage(FileSystemStorage):
