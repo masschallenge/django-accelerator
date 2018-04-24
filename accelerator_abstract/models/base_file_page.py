1d901a1d29119daa0872e413107d0ddd2aac119b
Yotam Manor <yotammanor@gmail.com>
2018-04-24 09:02:37 +0300

    change base_file_page to use the default storage.

--- accelerator_abstract/models/base_file_page.py
+++ accelerator_abstract/models/base_file_page.py
@@ -3,22 +3,14 @@
 
 from __future__ import unicode_literals
 
-from django.conf import settings
 from django.db import models
 from fluent_pages.models import Page
 
 from accelerator_abstract.models.accelerator_model import AcceleratorModel
-# customized storage that places CMS files outside of url space so we can
-# control access via the FilePage object to which they are attached.
-from accelerator_abstract.models.secure_file_system_storage import (
-    SecureFileSystemStorage
-)
-
-cms_fs = SecureFileSystemStorage(location=settings.CMS_FILE_ROOT)
 
 
 class BaseFilePage(Page):
-    file = models.FileField(storage=cms_fs, upload_to="%Y-%m")
+    file = models.FileField(upload_to="%Y-%m")
     description = models.TextField(blank=True)
 
     class Meta(AcceleratorModel.Meta):
