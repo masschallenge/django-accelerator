5c12611ebc830e29a3278f66e7249dc0fb4e18e9
Yotam Manor <yotammanor@gmail.com>
2018-04-24 11:34:37 +0300

    change BaseFilePage.file.storage to default_storage.

--- accelerator_abstract/models/base_file_page.py
+++ accelerator_abstract/models/base_file_page.py
@@ -7,10 +7,11 @@ from django.db import models
 from fluent_pages.models import Page
 
 from accelerator_abstract.models.accelerator_model import AcceleratorModel
+from django.conf import settings
 
 
 class BaseFilePage(Page):
-    file = models.FileField(upload_to="%Y-%m")
+    file = models.FileField(upload_to=settings.CMS_FILE_ROOT + "/%Y-%m")
     description = models.TextField(blank=True)
 
     class Meta(AcceleratorModel.Meta):
