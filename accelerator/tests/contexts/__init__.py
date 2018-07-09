a8bc4837a93eacfda0dabf6e44d08160ad98c148
Nathan Wilson <nathan@masschallenge.org>
2018-07-09 11:59:56 +0000

    Add AnalyzeJudgingContext

--- accelerator/tests/contexts/__init__.py
+++ accelerator/tests/contexts/__init__.py
@@ -1,3 +1,6 @@
+from accelerator.tests.contexts.analyze_judging_context import (
+    AnalyzeJudgingContext,
+)
 from accelerator.tests.contexts.judge_feedback_context import (
     JudgeFeedbackContext,
 )
