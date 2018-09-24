41fcd1e1b84f834dde32d1b894042ed9909edd36
Paul Bissex <paul@masschallenge.org>
2018-09-24 11:31:09 -0400

    [AC-5993] fleshed out clearance tests

--- accelerator/tests/test_clearance.py
+++ accelerator/tests/test_clearance.py
@@ -1,5 +1,10 @@
 from django.test import TestCase
 
+from accelerator.models import (
+    Clearance,
+    CLEARANCE_LEVEL_POM,
+    CLEARANCE_LEVEL_GLOBAL_MANAGER
+)
 from accelerator.tests.factories import ClearanceFactory
 
 
@@ -10,3 +15,23 @@ class TestClearance(TestCase):
         assert str(clearance.user) in str(clearance)
         assert str(clearance.program_family) in str(clearance)
         assert str(clearance.level) in str(clearance)
+
+    def test_higher_clearance_accepted(self):
+        clearance = ClearanceFactory(level=CLEARANCE_LEVEL_GLOBAL_MANAGER)
+        user = clearance.user
+        cleared = Clearance.objects.check_clearance(user, CLEARANCE_LEVEL_POM)
+        self.assertTrue(cleared)
+
+    def test_equal_clearance_accepted(self):
+        clearance = ClearanceFactory(level=CLEARANCE_LEVEL_GLOBAL_MANAGER)
+        user = clearance.user
+        cleared = Clearance.objects.check_clearance(
+            user, CLEARANCE_LEVEL_GLOBAL_MANAGER)
+        self.assertTrue(cleared)
+
+    def test_lower_clearance_rejected(self):
+        clearance = ClearanceFactory(level=CLEARANCE_LEVEL_POM)
+        user = clearance.user
+        cleared = Clearance.objects.check_clearance(
+            user, CLEARANCE_LEVEL_GLOBAL_MANAGER)
+        self.assertFalse(cleared)
