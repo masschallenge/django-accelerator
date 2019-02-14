c163bd2f0a2fee793730e792645e85c0c78fde5f
mkiterian <hmukite@gmail.com>
2019-02-14 16:55:41 +0300

    make sure tree field in navtree item if from our custom tree model

--- accelerator_abstract/models/base_nav_tree_item.py
+++ accelerator_abstract/models/base_nav_tree_item.py
@@ -2,6 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+import swapper
+
 from sitetree.models import TreeItemBase
 
 from accelerator_abstract.models.accelerator_model import AcceleratorModel
@@ -9,6 +11,8 @@ from accelerator_abstract.models.accelerator_model import AcceleratorModel
 
 class BaseNavTreeItem(TreeItemBase, AcceleratorModel):
     name = models.CharField(max_length=255)
+    tree = models.ForeignKey(to=swapper.get_model_name(
+        AcceleratorModel.Meta.app_label, "NavTree"))
 
     class Meta(AcceleratorModel.Meta):
         db_table = '{}_navtreeitem'.format(AcceleratorModel.Meta.app_label)
