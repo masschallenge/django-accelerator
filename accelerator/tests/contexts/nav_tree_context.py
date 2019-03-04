6c2016120f760febbda01ea3b22d9e9c0b8c5594
Vincent Asante <vincenthokie@gmail.com>
2019-03-04 11:33:00 +0300

    [AC-6380] - remove name field from base_nav_tree - remove name field from base_nav_tree_item model and add   permission fields - add nav_tree and nav_tree_item factories - create nav_tree context - add migration file

---
+++ accelerator/tests/contexts/nav_tree_context.py
@@ -0,0 +1,88 @@
+from accelerator.tests.factories import (
+    ExpertFactory,
+    NavTreeFactory,
+    NavTreeItemFactory,
+    ProgramFamilyFactory,
+    ProgramFactory,
+    ProgramRoleFactory,
+    ProgramRoleGrantFactory,
+    UserRoleFactory,
+)
+
+from accelerator.models import UserRole
+
+
+class NavTreeContext(object):
+    def __init__(self,
+                 tree=None,
+                 program_family=None,
+                 program=None,
+                 user_role=UserRole.STAFF,
+                 tree_user_role=UserRole.STAFF):
+
+        self.tree = tree
+        if not tree:
+            self.tree = NavTreeFactory()
+
+        self.user_role = UserRoleFactory(name=tree_user_role)
+
+        self.tree_item = NavTreeItemFactory(
+            tree=self.tree,
+            user_role=self.user_role)
+
+        self.program_family = program_family
+        if program_family is None:
+            self.program_family = ProgramFamilyFactory()
+
+        self.program = program
+        if program is None:
+            self.program = ProgramFactory(
+                program_family=self.program_family)
+
+        self.user = ExpertFactory(
+            profile__home_program_family=self.program_family)
+        self.program_role = ProgramRoleFactory(
+            program=self.program, user_role__name=user_role)
+        self.program_role_grant = ProgramRoleGrantFactory(
+            person=self.user,
+            program_role=self.program_role)
+
+    def add_program_to_tree_item(self, tree_item=None, program=None):
+
+        if tree_item is None:
+            tree_item = self.tree_item
+
+        if program is None:
+            program = self.program
+
+        tree_item.program.add(program)
+
+    def add_program_family_to_tree_item(self, tree_item=None, family=None):
+
+        if tree_item is None:
+            tree_item = self.tree_item
+
+        if family is None:
+            family = self.program_family
+
+        tree_item.program_family.add(family)
+
+    def add_excluded_program_to_tree_item(self, tree_item=None, program=None):
+
+        if tree_item is None:
+            tree_item = self.tree_item
+
+        if program is None:
+            program = self.program
+
+        tree_item.program_exclude.add(program)
+
+    def add_exculded_family_to_tree_item(self, tree_item=None, family=None):
+
+        if tree_item is None:
+            tree_item = self.tree_item
+
+        if family is None:
+            family = self.program_family
+
+        tree_item.program_family_exclude.add(family)
--- accelerator_abstract/models/base_nav_tree_item.py
+++ accelerator_abstract/models/base_nav_tree_item.py
@@ -1,18 +1,75 @@
 from __future__ import unicode_literals
 
-from django.db import models
-
 import swapper
-
+from django.db import models
 from sitetree.models import TreeItemBase
 
 from accelerator_abstract.models.accelerator_model import AcceleratorModel
 
 
 class BaseNavTreeItem(TreeItemBase, AcceleratorModel):
-    name = models.CharField(max_length=255)
+    """
+    A class used to represent a django sitetree item
+
+    Attributes
+    ----------
+    tree : obj
+        the NavTree object that this item belongs to
+    user_role : obj
+        the UserRole allowed to access this tree item, a null
+        here implies that we allow all UserRoles to access
+        this item (default null)
+    program_family : objs
+        the ProgramFamilies allowed to access this tree item, a blank
+        here implies that we allow all ProgramFamilies to access
+        this item (default '')
+    program : objs
+        the Programs allowed to access this tree item, a blank
+        here implies that we allow all Programs to access
+        this item (default '')
+    active_program : bool
+        determines if the programs that access this item must be active
+        (default False)
+    program_exclude : int
+        the Programs not allowed to access this tree item, a blank
+        here implies that we don't exclude any Programs from accessing
+        this item (default '')
+    program_family_exclude : str
+        the Programs not allowed to access this tree item, a blank
+        here implies that we do not exclude any Programs from accessing
+        this item (default '')
+    """
     tree = models.ForeignKey(to=swapper.get_model_name(
         AcceleratorModel.Meta.app_label, "NavTree"))
+    user_role = models.ForeignKey(
+        swapper.get_model_name(
+            AcceleratorModel.Meta.app_label, "UserRole"), null=True)
+    program_family = models.ManyToManyField(
+        to=swapper.get_model_name(
+            AcceleratorModel.Meta.app_label, 'ProgramFamily'),
+        blank=True)
+    program = models.ManyToManyField(
+        to=swapper.get_model_name(
+            AcceleratorModel.Meta.app_label, 'Program'),
+        blank=True)
+    active_program = models.BooleanField(default=False)
+    '''
+        added to allow
+         - 'show for all program families exluding program family Y'
+         - 'show for program family X exluding program Y'
+         - 'show to all programs exluding programs A, B, C'
+        level filtering
+    '''
+    program_exclude = models.ManyToManyField(
+        to=swapper.get_model_name(
+            AcceleratorModel.Meta.app_label, 'Program'),
+        blank=True,
+        related_name='program_exlusion')
+    program_family_exclude = models.ManyToManyField(
+        to=swapper.get_model_name(
+            AcceleratorModel.Meta.app_label, 'ProgramFamily'),
+        blank=True,
+        related_name='program_family_exlusion')
 
     class Meta(AcceleratorModel.Meta):
         db_table = '{}_navtreeitem'.format(AcceleratorModel.Meta.app_label)
