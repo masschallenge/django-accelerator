8d5b16ad82f9f1f177ef67458876d0b16ed7419f
Vincent Asante <vincenthokie@gmail.com>
2019-03-05 14:59:32 +0300

    fix lint complaints

--- accelerator/migrations/0039_make_user_role_blankable_in_nav_tree_item.py
+++ accelerator/migrations/0039_make_user_role_blankable_in_nav_tree_item.py
@@ -10,13 +10,18 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accelerator', '0038_add_permissions_fields_to_nav_tree_item_model'),
+        (
+            'accelerator',
+            '0038_add_permissions_fields_to_nav_tree_item_model'),
     ]
 
     operations = [
         migrations.AlterField(
             model_name='navtreeitem',
             name='user_role',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.ACCELERATOR_USERROLE_MODEL),
+            field=models.ForeignKey(
+                blank=True,
+                null=True, on_delete=django.db.models.deletion.CASCADE,
+                to=settings.ACCELERATOR_USERROLE_MODEL),
         ),
     ]
