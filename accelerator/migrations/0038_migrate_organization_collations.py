6e687b830dbe2cb07ad04be05ca2abc58724b2ab
shakirandagire <ndagire.shakira@gmail.com>
2019-03-06 22:29:59 +0300

    Fixing AC-6457

---
+++ accelerator/migrations/0038_migrate_organization_collations.py
@@ -0,0 +1,18 @@
+from django.db import migrations
+
+def get_conversion_function(collation, character_set='utf8'):
+
+	def alter_collation_from_migration(apps, schema_editor):
+		with schema_editor.connection.cursor() as cursor:
+			cursor.execute('ALTER TABLE accelerator_organization CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci')
+	return alter_collation_from_migration
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accelerator', '0037_change_help_texts_for_notes_and_other_mentors_fields'),
+    ]
+    operations = [
+        migrations.RunPython(get_conversion_function('utf8mb4_general_ci')),
+    ]
