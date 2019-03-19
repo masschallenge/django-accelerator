c5131ab293aeb779e87e575659b3828685768a4f
shakirandagire <ndagire.shakira@gmail.com>
2019-03-19 19:54:23 +0300

    Correct the linting issues in the file

--- accelerator/migrations/0042_migrate_organization_collations.py
+++ accelerator/migrations/0042_migrate_organization_collations.py
@@ -1,11 +1,13 @@
 from django.db import migrations
 
-def get_conversion_function(collation, character_set='utf8'):
 
-	def alter_collation_from_migration(apps, schema_editor):
-		with schema_editor.connection.cursor() as cursor:
-			cursor.execute('ALTER TABLE accelerator_organization CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci')
-	return alter_collation_from_migration
+def get_conversion_function(collation, character_set='utf8'):
+    def alter_collation_from_migration(apps, schema_editor):
+        with schema_editor.connection.cursor() as cursor:
+            cursor.execute(
+                'ALTER TABLE accelerator_organization'
+                'CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci')
+    return alter_collation_from_migration
 
 
 class Migration(migrations.Migration):
