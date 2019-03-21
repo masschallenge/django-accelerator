486f1e519fef39f153c6c7c71271a9b1358e81df
mkiterian <hmukite@gmail.com>
2019-03-21 19:26:29 +0300

    move side nav tree alias to base class

--- accelerator_abstract/models/base_nav_tree.py
+++ accelerator_abstract/models/base_nav_tree.py
@@ -5,6 +5,9 @@ from sitetree.models import TreeBase
 from accelerator_abstract.models.accelerator_model import AcceleratorModel
 
 
+MC_SIDE_NAV_TREE_ALIAS = 'mc_side_nav_tree'
+
+
 class BaseNavTree(TreeBase, AcceleratorModel):
     class Meta(AcceleratorModel.Meta):
         db_table = '{}_navtree'.format(AcceleratorModel.Meta.app_label)
