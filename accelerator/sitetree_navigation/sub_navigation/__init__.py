c768c28d116e4a6849ed7163d579cb83cc39c3f5
Vincent Asante <vincenthokie@gmail.com>
2019-03-29 00:44:37 +0300

    rename migrations and revert __init__ imports

--- accelerator/sitetree_navigation/sub_navigation/__init__.py
+++ accelerator/sitetree_navigation/sub_navigation/__init__.py
@@ -1,20 +1,18 @@
-from accelerator.sitetree_navigation import sub_navigation
-
-from sub_navigation.directory_subnav_definition import (
+from accelerator.sitetree_navigation.sub_navigation.directory_subnav_definition import (
     create_directory_subnav
 )
-from sub_navigation.event_subnav_definition import (
+from accelerator.sitetree_navigation.sub_navigation.event_subnav_definition import (
     create_events_subnav
 )
-from sub_navigation.home_subnav_definition import (
+from accelerator.sitetree_navigation.sub_navigation.home_subnav_definition import (
     create_home_subnav
 )
-from sub_navigation.judging_subnav_definition import (
+from accelerator.sitetree_navigation.sub_navigation.judging_subnav_definition import (
     create_judging_subnav
 )
-from sub_navigation.resources_subnav_definition import (
+from accelerator.sitetree_navigation.sub_navigation.resources_subnav_definition import (
     create_resources_subnav
 )
-from sub_navigation.startup_dashboard_subnav_definition import (
+from accelerator.sitetree_navigation.sub_navigation.startup_dashboard_subnav_definition import (
     create_startup_dashboard_subnav
 )
