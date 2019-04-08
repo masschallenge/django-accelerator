e8965f53ae635969973defbedb980b4913069ce9
Vincent Asante <vincenthokie@gmail.com>
2019-04-08 22:47:09 +0300

    rename file with wrong name

--- accelerator_abstract/models/__init__.py
+++ accelerator_abstract/models/__init__.py
@@ -330,4 +330,4 @@ from .base_user_legal_check import BaseUserLegalCheck
 from .base_nav_tree import BaseNavTree
 from .base_nav_tree_item import BaseNavTreeItem
 
-from .base_fluent_side_nav_association import BaseNodeSubNavAssociation
+from .base_fluent_sub_nav_association import BaseNodeSubNavAssociation
