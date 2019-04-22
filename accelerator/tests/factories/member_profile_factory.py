f4bdcf21460048e18465483ab208e0484e88779b
Vincent Asante <vincenthokie@gmail.com>
2019-04-22 14:43:05 -0400

    [AC-6622] remove duplicate logic

--- accelerator/tests/factories/member_profile_factory.py
+++ accelerator/tests/factories/member_profile_factory.py
@@ -14,11 +14,3 @@ MemberProfile = swapper.load_model(AcceleratorConfig.name, 'MemberProfile')
 class MemberProfileFactory(CoreProfileFactory):
     class Meta:
         model = MemberProfile
-
-    @classmethod
-    def _create(cls, model_class, *args, **kwargs):
-        profile = model_class.objects.filter(user=kwargs['user'])
-        if profile:
-            profile.delete()
-        manager = cls._get_manager(model_class)
-        return manager.create(*args, **kwargs)
