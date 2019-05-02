48e02c9d22cd079b563a347b293e83c9a0ed1f38
geofrocker <geofrocker2@gmail.com>
2019-05-02 11:46:26 -0400

    Add missing lines

--- accelerator/migrations/0051_add_register_for_events_to_event_subnav_items.py
+++ accelerator/migrations/0051_add_register_for_events_to_event_subnav_items.py
@@ -7,8 +7,10 @@ from accelerator.sitetree_navigation.sub_navigation import (
     create_events_subnav,
 )
 
+
 def create_subnav_trees_and_items(apps, schema_editor):
     create_events_subnav()
+
 
 class Migration(migrations.Migration):
 
