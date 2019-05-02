6b02cf48a246a938f27d99a5f818858c61166893
geofrocker <geofrocker2@gmail.com>
2019-05-02 10:26:20 -0400

    remove unnecessary imports

--- accelerator/migrations/0051_add_register_for_events_to_event_subnav_items.py
+++ accelerator/migrations/0051_add_register_for_events_to_event_subnav_items.py
@@ -4,23 +4,11 @@ from __future__ import unicode_literals
 from django.db import migrations
 
 from accelerator.sitetree_navigation.sub_navigation import (
-    create_directory_subnav,
     create_events_subnav,
-    create_home_subnav,
-    create_judging_subnav,
-    create_resources_subnav,
-    create_startup_dashboard_subnav
 )
 
-
 def create_subnav_trees_and_items(apps, schema_editor):
-    create_directory_subnav()
     create_events_subnav()
-    create_home_subnav()
-    create_judging_subnav()
-    create_resources_subnav()
-    create_startup_dashboard_subnav()
-
 
 class Migration(migrations.Migration):
 
