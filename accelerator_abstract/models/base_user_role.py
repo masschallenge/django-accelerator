68c2e6786005b119b2c9dc718df862deb064b71a
geofrocker <geofrocker2@gmail.com>
2019-06-14 21:12:11 +0300

    Modify the has_user_role_base function to accomodate the active_or_ended_programs variable

--- accelerator_abstract/models/base_user_role.py
+++ accelerator_abstract/models/base_user_role.py
@@ -50,14 +50,19 @@ class BaseUserRole(AcceleratorModel):
 
 
 def has_user_role_base(
-        user, user_role_name, program=None, inactive_programs=False):
+        user, user_role_name, program=None,
+        inactive_programs=False, active_or_ended_programs=False):
     filter = user.programrolegrant_set.filter(
         program_role__user_role__name=user_role_name)
     if program:
         filter = filter.filter(program_role__program=program)
     if not inactive_programs:
-        filter = filter.filter(program_role__program__program_status__in=[
+        filter=filter.filter(program_role__program__program_status__in=[
             "active", "upcoming"])
+        return filter.exists()
+    if inactive_programs and active_or_ended_programs:
+        filter=filter.filter(program_role__program__program_status__in=[
+            "active", "ended"])
     return filter.exists()
 
 
