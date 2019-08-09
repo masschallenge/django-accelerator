7c0ae9e92a56be3d57ddf0c5658b913b3d99bb73
Felix Kitaka <webash.uganda@gmail.com>
2019-08-09 21:53:52 +0300

    [AC-6946] Improved location fields help texts.

--- accelerator_abstract/models/base_location.py
+++ accelerator_abstract/models/base_location.py
@@ -8,19 +8,43 @@ from accelerator_abstract.models.accelerator_model import AcceleratorModel
 
 
 class BaseLocation(AcceleratorModel):
-    name = CharField(max_length=100, blank=False)
-    city = CharField(max_length=100, blank=True, default='')
-    state = CharField(max_length=100, blank=True, default='')
-    country = CharField(max_length=100, blank=True, default='')
-    postcode = CharField(max_length=20, blank=True, default='')
-    latitude = CharField(max_length=100, blank=True, default='')
-    longitude = CharField(max_length=100, blank=True, default='')
+    name = CharField(
+        max_length=100,
+        blank=False)
+    city = CharField(
+        max_length=100,
+        blank=True,
+        default="")
+    state = CharField(
+        max_length=100,
+        blank=True,
+        default="",
+        help_text="State/Region/Province"
+    )
+    country = CharField(
+        max_length=100,
+        blank=True,
+        default="")
+    postcode = CharField(
+        max_length=20,
+        blank=True,
+        default="")
+    latitude = CharField(
+        max_length=100,
+        blank=True,
+        default="")
+    longitude = CharField(
+        max_length=100,
+        blank=True,
+        default="")
     timezone = CharField(
         max_length=35,
         blank=False,
         default="UTC",
         help_text=("Timezone name from Olson Timezone database "
-                   "(https://en.wikipedia.org/wiki/Tz_database)")
+                   "(https://en.wikipedia.org/wiki/Tz_database, "
+                   "https://en.wikipedia.org/wiki/"
+                   "List_of_tz_database_time_zones)")
     )
 
     class Meta(AcceleratorModel.Meta):
