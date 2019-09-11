1847a6197167f17a42359ad67a0ed8134ae4f30e
shakirandagire <ndagire.shakira@gmail.com>
2019-09-11 15:06:23 +0300

    [AC-7049] Add tests for the functionality added

--- accelerator/tests/test_program_cycle.py
+++ accelerator/tests/test_program_cycle.py
@@ -12,3 +12,22 @@ class TestProgramCycle(TestCase):
     def test_display_name_no_short_name(self):
         cycle = ProgramCycleFactory(short_name=None)
         assert cycle.name in str(cycle)
+
+    def test_program_cycle_with_open_applications_has_default_application_type(self):
+        cycle = ProgramCycleFactory()
+        if (cycle.applications_open and 
+        not cycle.default_application_type):
+            self.assertRaises("Open applications must have a default application type.")
+
+    def test_program_cycle_with_open_applications_has_default_application_type_and_associated_programs(self):
+        cycle = ProgramCycleFactory()
+        if (cycle.applications_open and 
+        not cycle.default_application_type
+        and cycle.programs.exists()):
+            self.assertRaises("Default application type can’t be removed"
+                              "from the cycle until the program cycle is"
+                              "disassociated with all programs")
+    
+    
+
+
