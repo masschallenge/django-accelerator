c386d2ddd2de75562ec6ac8c326f15f7378def7f
Jon Kiparsky <jon@kiparsky.net>
2019-09-26 17:01:49 -0400

    [AC-6976] Fix dependency name

--- accelerator/migrations/0002_add_street_address_to_location.py
+++ accelerator/migrations/0002_add_street_address_to_location.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accelerator', '0001_initial.py')
+        ('accelerator', '0001_initial')
     ]
 
     operations = [
