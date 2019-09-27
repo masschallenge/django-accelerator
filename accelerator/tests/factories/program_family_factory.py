34e1e96ce6a0b0fcbe7b9db5e4f7958dc32f567f
Jon Kiparsky <jon@kiparsky.net>
2019-09-27 16:26:20 -0400

    [AC-6976b] Remove address and timezone from ProgramFamily

--- accelerator/tests/factories/program_family_factory.py
+++ accelerator/tests/factories/program_family_factory.py
@@ -23,7 +23,6 @@ class ProgramFamilyFactory(DjangoModelFactory):
     url_slug = Sequence(lambda n: "pf{0}".format(n))
     email_domain = Sequence(lambda n: "pf{0}.accelerator.org".format(n))
     phone_number = "617-555-1212"
-    physical_address = "Boston"
     is_open_for_startups = True
     is_open_for_experts = True
     use_site_tree_side_nav = False
