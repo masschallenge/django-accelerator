762fa57fb71d50dd690f4905f15a5c5352af927f
Jon Kiparsky <jon@kiparsky.net>
2019-10-10 12:10:08 -0400

    [AC-6976b] Rename migrations

--- accelerator/migrations/0007_remove_program_family_address_and_timezone.py
+++ accelerator/migrations/0007_remove_program_family_address_and_timezone.py
@@ -8,7 +8,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accelerator', '0005_make_location_nullable'),
+        ('accelerator', '0006_remove_userrole_url_slug'),
     ]
 
     operations = [
