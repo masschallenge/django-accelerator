feff02c6ba4cf8a5b044ee88ec93338a237de950
Felix Kitaka <webash.uganda@gmail.com>
2019-10-16 21:28:14 +0300

    [AC-7072] Added a shared localized_today_utc_date function.

--- accelerator/utils.py
+++ accelerator/utils.py
@@ -1,3 +1,12 @@
+from datetime import (
+    date,
+    datetime,
+    time,
+)
+from pytz import (
+    timezone,
+    utc,
+)
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.models import Permission
 
@@ -14,3 +23,10 @@ def create_mc_permission(permission):
         group.permissions.add(new_perm)
     for user in permission.user_set.all():
         user.user_permissions.add(new_perm)
+
+
+def localized_today_utc_date(_timezone):
+    tz = timezone(_timezone)
+    today = tz.localize(datetime.combine(
+        date.today(), time(0))).astimezone(utc)
+    return today
