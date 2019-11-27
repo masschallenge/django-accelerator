2e8059e253000c6908bb26e50f4ff22d86a845d5
Shankar Ambady <shanbady@gmail.com>
2019-11-27 14:55:39 -0500

    bumping up min version and setting a max

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-DJANGO_SPEC = ">=1.8"
+DJANGO_SPEC = ">=1.11,<=2.3"
 if "DJANGO_VERSION" in os.environ:
     DJANGO_SPEC = "=={}".format(os.environ["DJANGO_VERSION"])
 
