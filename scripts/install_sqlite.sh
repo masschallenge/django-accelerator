e900dc0c1d3e33def89906e2966bd413c442ed7d
Philip Kwesiga <phil.kwesiga@gmail.com>
2019-12-04 20:26:35 +0300

    [AC-7030] install sqlite3 on Travis from ppa

--- scripts/install_sqlite.sh
+++ scripts/install_sqlite.sh
@@ -9,11 +9,8 @@ remove_older_version(){
 
 install_sqlite(){
     # install sqlite 3.29.0
-    wget https://www3.sqlite.org/cgi/src/tarball/fc82b73eaa/SQLite-fc82b73eaa.tar.gz
-    tar -zxvf SQLite-fc82b73eaa.tar.gz
-    cd SQLite-fc82b73eaa
-    ./configure && make && sudo make install
-    sudo cp sqlite3 /usr/bin/sqlite3
+    add-apt-repository ppa:jonathonf/backports && \
+    apt-get -y update && sudo apt-get install -y sqlite3 && \
     echo 'export LD_LIBRARY_PATH="/usr/local/lib"' >> ~/.bashrc
     . ~/.bashrc
     echo "======= sqlite version updated ======="
