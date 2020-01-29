b2723169585cf8edce258774374ecf2861dba0ec
Jon Kiparsky <jon@kiparsky.net>
2020-01-29 15:30:12 -0500

    [AC-7310] Add JudgingRoundContext

---
+++ accelerator/tests/contexts/judging_round_context.py
@@ -0,0 +1,36 @@
+from accelerator.tests.factories import (
+    JudgingFormFactory,
+    JudgingFormElementFactory,
+    JudgingRoundFactory,
+)
+from accelerator_abstract.models import FORM_ELEM_OVERALL_RECOMMENDATION
+
+class JudgingRoundContext:
+    def __init__(self, **kwargs):
+        if kwargs.get(is_active) is True:
+            should_be_active = True
+            kwargs[is_active] = False
+        else:
+            should_be_active = False
+        self.judging_round = JudgingRoundFactory(**kwargs)
+        if should_be_active:
+            self.activate_judging_round()
+
+
+    def activate_judging_round(self):
+        self.judging_form = prepare_judging_form()
+        self.judging_round.judging_form = self.judging_form
+        self.judging_round.is_active=True
+        self.judging_round.save()
+        
+
+    def prepare_judging_form():
+        judging_form = JudgingFormFactory()
+        if include_overall_recommendation():
+            JudgingFormElementFactory(
+                element_name=FORM_ELEM_OVERALL_RECOMMENDATION,
+                form_type=self.judging_form)
+        return judging_form
+
+
+            
