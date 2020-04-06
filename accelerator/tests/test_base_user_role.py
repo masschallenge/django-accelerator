5d1eab1488d7a9417025eccd311825e982d5fd50
Ian Kigen <ian.Gabe.ian@gmail.com>
2020-04-06 18:27:53 +0300

    [AC-7517] add has_user_roles test

--- accelerator/tests/test_base_user_role.py
+++ accelerator/tests/test_base_user_role.py
@@ -23,8 +23,14 @@ class TestBaseUserRole(TestCase):
                                            inactive_programs=True,
                                            active_or_ended_programs=True))
 
-    def test_has_user_roles(self):
+    def test_has_user_roles_is_true_when_role_exists(self):
         context = UserRoleContext(user_role_name=UserRole.FINALIST)
         user = context.user
         self.assertTrue(has_user_roles(user,
                                        [UserRole.ALUM, UserRole.FINALIST]))
+
+    def test_has_user_roles_is_false_when_role_does_not_exists(self):
+        context = UserRoleContext(user_role_name=UserRole.MENTOR)
+        user = context.user
+        self.assertFalse(has_user_roles(user,
+                                        [UserRole.ALUM, UserRole.FINALIST]))
