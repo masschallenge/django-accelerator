325283be47a2ae8bc3ab86c72b17f6222520ad4b
Shakira Ndagire <ndagire.shakira@gmail.com>
2020-04-21 19:55:00 +0300

    [AC-7594] Fix linting issues

--- accelerator/migrations/0019_add_deferred_user_role.py
+++ accelerator/migrations/0019_add_deferred_user_role.py
@@ -12,20 +12,20 @@ def add_deferred_user_role(apps, schema_editor):
         user_role = UserRole.objects.filter(user=DEFERRED_MENTOR)[0]
     else:
         user_role = UserRole.objects.create(name=DEFERRED_MENTOR,
-                                sort_order=17)
+                                            sort_order=17)
     for program in Program.objects.all():
         if not ProgramRole.objects.filter(user_role=user_role,
-                                      program=program).exists():
+                                          program=program).exists():
             name = "{} {} ({}-{})".format(
-            (program.end_date.year if program.end_date else ""),
-            DEFERRED_MENTOR,
-            program.program_family.url_slug.upper(),
-            program.pk)
+                (program.end_date.year if program.end_date else ""),
+                DEFERRED_MENTOR,
+                program.program_family.url_slug.upper(),
+                program.pk)
 
             ProgramRole.objects.get_or_create(
-            program=program,
-            user_role=user_role,
-            name=name)
+                program=program,
+                user_role=user_role,
+                name=name)
 
 
 class Migration(migrations.Migration):
