20a31f70c47593c043d4425a187b57cad475ec37
Geofrey Asiimwe <geofrocker2@gmail.com>
2020-06-05 14:55:52 +0300

    [AC-7818] Add meeting_info to the base_mentor_program_office_hour.py file

--- accelerator_abstract/models/base_mentor_program_office_hour.py
+++ accelerator_abstract/models/base_mentor_program_office_hour.py
@@ -49,6 +49,7 @@ class BaseMentorProgramOfficeHour(AcceleratorModel):
         on_delete=models.CASCADE)
     notify_reservation = models.BooleanField(default=True)
     topics = models.CharField(max_length=500, blank=True)
+    meeting_info = models.CharField(max_length=100, blank=True, default="")
 
     class Meta(AcceleratorModel.Meta):
         db_table = '{}_mentorprogramofficehour'.format(
