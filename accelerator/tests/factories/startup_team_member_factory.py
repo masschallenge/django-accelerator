b940c69dc4b6115a102c2bf5fbb12dde25634f0b
Shankar Ambady <shanbady@gmail.com>
2020-06-08 20:01:23 -0400

    [AC-7833] fixing tests

--- accelerator/tests/factories/startup_team_member_factory.py
+++ accelerator/tests/factories/startup_team_member_factory.py
@@ -4,7 +4,6 @@
 from __future__ import unicode_literals
 
 import swapper
-import factory
 from factory import (
     DjangoModelFactory,
     Sequence,
@@ -16,33 +15,12 @@ from accelerator.tests.factories.entrepreneur_factory import (
     EntrepreneurFactory
 )
 from accelerator.tests.factories.startup_factory import StartupFactory
-from mc.signals import (
-    added_startup_team_member,
-    delete_partner_member,
-    edited_startup_profile,
-    paypal_payment_refunded,
-    refund_code_applied,
-    refund_code_failed,
-    removed_program_interest,
-    removed_startup_team_member,
-    saved_program_interest,
-    startup_created,
-    sync_startup_members,
-    update_partner_member,
-    user_attempted_payment,
-    startup_team_member_role_unset,
-    startup_team_member_role_set,
-    user_failed_to_register_for_earlybird,
-    user_successfully_registered_for_earlybird,
-)
-from django.db.models.signals import pre_save, post_save
 
 StartupTeamMember = swapper.load_model(
     AcceleratorConfig.name,
     'StartupTeamMember')
 
 
-@factory.django.mute_signals(pre_save, post_save)
 class StartupTeamMemberFactory(DjangoModelFactory):
     class Meta:
         model = StartupTeamMember
