d735090c9cfe101fb3699705137e813d660edfb8
Shankar Ambady <shanbady@gmail.com>
2020-10-20 12:27:30 -0400

    removing use of get_app_name

--- accelerator/tests/factories/content_type_factory.py
+++ accelerator/tests/factories/content_type_factory.py
@@ -8,12 +8,10 @@ from django.contrib.contenttypes.models import ContentType
 from factory import Sequence
 from factory.django import DjangoModelFactory
 
-from accelerator.tests.utils import get_app_name
-
 
 class ContentTypeFactory(DjangoModelFactory):
     class Meta:
         model = ContentType
 
-    app_label = get_app_name()
+    app_label = 'accelerator'
     model = Sequence(lambda n: "test_contenttypemodel{0}".format(n))
