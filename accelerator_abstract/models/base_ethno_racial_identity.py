8dd39ab7eb23e7b428868327d704e9ad690920ee
iankigen <ian.gabe.ian@gmail.com>
2020-12-01 21:43:36 +0300

    [AC-8366] lint

--- accelerator_abstract/models/base_ethno_racial_identity.py
+++ accelerator_abstract/models/base_ethno_racial_identity.py
@@ -6,7 +6,8 @@ BLACK = 'Black'
 EAST_SOUTHEAST_ASIAN = 'East and/or Southeast Asian'
 HISPANIC_LATINX_SPANISH = 'Hispanic, Latinx, and/or Spanish'
 INDIGENOUS = 'Indigenous'
-HAWAIIAN_PACIFIC_POLYNESIAN = 'Native Hawaiian, Pacific Islander, or Polynesian'
+HAWAIIAN_PACIFIC_POLYNESIAN = ('Native Hawaiian, '
+                               'Pacific Islander, or Polynesian')
 MIDDLE_EASTERN_NORTH_AFRICAN = 'Middle Eastern and/or North African'
 NORTH_CENTRAL_ASIAN = 'North and/or Central Asian'
 SOUTH_ASIAN = 'South Asian'
@@ -16,10 +17,13 @@ PREFER_NOT_TO_STATE = 'I prefer not to say'
 ETHNO_RACIAL_IDENTITY_CHOICES = [
     (BLACK, 'e.g., African American, Haitian, Nigerian, Somali'),
     (EAST_SOUTHEAST_ASIAN, 'e.g., Chinese, Filipino, Korean, Indonesian'),
-    (HISPANIC_LATINX_SPANISH, 'e.g., Brazilian, Colombian, Mexican, Nicaraguan'),
+    (HISPANIC_LATINX_SPANISH,
+     'e.g., Brazilian, Colombian, Mexican, Nicaraguan'),
     (INDIGENOUS, 'e.g., Aboriginal, Inuit, Maasai, Native American'),
-    (HAWAIIAN_PACIFIC_POLYNESIAN, 'Native Hawaiian, Pacific Islander, or Polynesian'),
-    (MIDDLE_EASTERN_NORTH_AFRICAN, 'e.g., Egyptian, Emirati, Iranian, Moroccan'),
+    (HAWAIIAN_PACIFIC_POLYNESIAN,
+     'Native Hawaiian, Pacific Islander, or Polynesian'),
+    (MIDDLE_EASTERN_NORTH_AFRICAN,
+     'e.g., Egyptian, Emirati, Iranian, Moroccan'),
     (NORTH_CENTRAL_ASIAN, 'e.g., Kazakh, Siberian, Tajik, Uzbek'),
     (SOUTH_ASIAN, 'e.g., Bangladeshi, Indian, Pakistani, Sri Lankan'),
     (WHITE_CAUCASIAN, 'White or Caucasian'),
