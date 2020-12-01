f561fc41fe71a5c461f9060c03491643099ccef0
iankigen <ian.gabe.ian@gmail.com>
2020-12-01 17:19:15 +0300

    [AC-8366] add data migration

---
+++ accelerator/migrations/0028_add_ethno_racial_identity_data.py
@@ -0,0 +1,23 @@
+import swapper
+from django.db import migrations
+
+from accelerator_abstract.models.base_ethno_racial_identity import (
+    ETHNO_RACIAL_IDENTITY_CHOICES,
+)
+
+
+def add_ethno_racial_identity_data(*args, **kwargs):
+    EthnoRacialIdentity = swapper.load_model('accelerator', 'EthnoRacialIdentity')
+    for ethno_racial_identity in ETHNO_RACIAL_IDENTITY_CHOICES:
+        EthnoRacialIdentity.objects.create(identity=ethno_racial_identity[0])
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('accelerator', '0027_add_ethno_racial_identity_model'),
+    ]
+
+    operations = [
+        migrations.RunPython(add_ethno_racial_identity_data,
+                             migrations.RunPython.noop)
+    ]
