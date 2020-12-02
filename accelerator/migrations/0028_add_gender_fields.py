42b9e62be7eb0ce7ef60849f04d4296e08d75e31
Geofrey Asiimwe <geofrocker2@gmail.com>
2020-12-02 12:49:13 +0300

    [AC-8362] Add gender choices fields

---
+++ accelerator/migrations/0028_add_gender_fields.py
@@ -0,0 +1,50 @@
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accelerator', '0027_add_gender_choices_object'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='entrepreneurprofile',
+            name='gender_self_description',
+            field=models.TextField(blank=True, default=''),
+        ),
+        migrations.AddField(
+            model_name='entrepreneurprofile',
+            name='gender_identity',
+            field=models.ManyToManyField(
+                blank=True,
+                to=settings.ACCELERATOR_GENDERCHOICES_MODEL),
+        ),
+        migrations.AddField(
+            model_name='expertprofile',
+            name='gender_self_description',
+            field=models.TextField(blank=True, default=''),
+        ),
+        migrations.AddField(
+            model_name='expertprofile',
+            name='gender_identity',
+            field=models.ManyToManyField(
+                blank=True,
+                to=settings.ACCELERATOR_GENDERCHOICES_MODEL),
+        ),
+        migrations.AddField(
+            model_name='memberprofile',
+            name='gender_self_description',
+            field=models.TextField(blank=True, default=''),
+        ),
+        migrations.AddField(
+            model_name='memberprofile',
+            name='gender_identity',
+            field=models.ManyToManyField(
+                blank=True,
+                to=settings.ACCELERATOR_GENDERCHOICES_MODEL),
+        ),
+    ]
