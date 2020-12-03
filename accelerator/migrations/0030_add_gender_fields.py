1f6955eed48669b7bb0680161c8da0f4e82dda4d
Geofrey Asiimwe <geofrocker2@gmail.com>
2020-12-03 20:21:22 +0300

    [AC-8362] Rename migrations

--- accelerator/migrations/0030_add_gender_fields.py
+++ accelerator/migrations/0030_add_gender_fields.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accelerator', '0027_add_gender_choices_object'),
+        ('accelerator', '0029_add_gender_choices_data'),
     ]
 
     operations = [
