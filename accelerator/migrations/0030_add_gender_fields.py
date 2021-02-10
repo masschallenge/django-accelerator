9aa9ad287573f84ba57277335c72afec3cdbc17b
Paul Bissex <paul@masschallenge.org>
2021-02-10 14:49:41 -0500

    [AC-8380] fixed migration dependency

--- accelerator/migrations/0030_add_gender_fields.py
+++ accelerator/migrations/0030_add_gender_fields.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accelerator', '0001_initial'),
+        ('accelerator', '0029_add_gender_choices_data'),
     ]
 
     operations = [
