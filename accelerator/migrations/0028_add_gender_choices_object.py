34e550f3c27a1189965c11b52e434c48f32651f1
Paul Bissex <paul@masschallenge.org>
2021-02-10 15:51:13 -0500

    [AC-8380] set managed=True in intial migration

--- accelerator/migrations/0028_add_gender_choices_object.py
+++ accelerator/migrations/0028_add_gender_choices_object.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accelerator', '0002_set_swappable_to_none_and_managed_to_true'),
+        ('accelerator', '0001_initial'),
     ]
 
     operations = [
