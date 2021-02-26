69c1b0ac0ace454f917060f49f9b3971deded9c9
Ian Kigen <ian.gabe.ian@gmail.com>
2021-02-26 15:50:39 +0300

    [AC-8417] update deferrable model

--- accelerator/models/deferrable_modal.py
+++ accelerator/models/deferrable_modal.py
@@ -1,9 +1,10 @@
 import swapper
-from accelerator_abstract.models import BaseDeferrableModal
+from accelerator_abstract.models import (
+    BaseDeferrableModal,
+)
 
 
 class DeferrableModal(BaseDeferrableModal):
-
     class Meta(BaseDeferrableModal.Meta):
         swappable = swapper.swappable_setting(
             BaseDeferrableModal.Meta.app_label, 'DeferrableModal'
