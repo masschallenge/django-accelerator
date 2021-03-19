713d8f8a48a9bf0d2acf56071344b939a63698d2
Paul Bissex <paul@masschallenge.org>
2021-03-19 14:13:17 -0400

    fixed swapper errors in test_get_profile by importing models from mc

--- accelerator/tests/test_get_profile.py
+++ accelerator/tests/test_get_profile.py
@@ -11,7 +11,7 @@ try:
 except ImportError:
     from mock import patch
 
-from accelerator.models import (
+from mc.models import (
     BaseProfile,
     ENTREPRENEUR_USER_TYPE,
     EXPERT_USER_TYPE,
