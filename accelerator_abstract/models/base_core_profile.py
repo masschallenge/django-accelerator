28666cd41fe67aa100186aea2842d613b6b37ba0
Jon Kiparsky <jon@kiparsky.net>
2021-03-23 10:48:22 -0400

    [AC-8173] Remove expert_nav feature-flagging

--- accelerator_abstract/models/base_core_profile.py
+++ accelerator_abstract/models/base_core_profile.py
@@ -55,7 +55,6 @@ UI_GENDER_CHOICES = (
 )
 JUDGE_FIELDS_TO_LABELS = {'desired_judge_label': 'Desired Judge',
                           'confirmed_judge_label': 'Judge'}
-EXPERT_NAVIGATION_EPIC = "expert_navigation"
 
 
 class BaseCoreProfile(AcceleratorModel):
@@ -234,9 +233,8 @@ class BaseCoreProfile(AcceleratorModel):
             return '/staff'
 
     def role_based_landing_page(self, exclude_role_names=[]):
-        if flag_smith_has_feature(EXPERT_NAVIGATION_EPIC):
-            if self.user_type.upper() == EXPERT_USER_TYPE:
-                return "/dashboard/expert/overview/"
+        if self.user_type.upper() == EXPERT_USER_TYPE:
+            return "/dashboard/expert/overview/"
         JudgingRound = swapper.load_model(AcceleratorModel.Meta.app_label,
                                           "JudgingRound")
         UserRole = swapper.load_model(
