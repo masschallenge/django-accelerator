8606ac7379230fa4be2af7311474d59cc2332beb
kenware <ejykken@gmail.com>
2021-07-21 18:46:28 +0100

    [AC-9014] implement feedbacks

--- accelerator_abstract/models/base_note.py
+++ accelerator_abstract/models/base_note.py
@@ -7,6 +7,7 @@ from accelerator_abstract.models.accelerator_model import AcceleratorModel
 
 class BaseNote(AcceleratorModel):
     author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               related_name="%(class)s_notes",
                                on_delete=models.CASCADE)
     note_content = models.TextField(default='',
                                     help_text='Note contents')
