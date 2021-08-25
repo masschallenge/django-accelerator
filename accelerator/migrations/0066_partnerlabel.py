9da48bb3a0bc8dc1e495a0fe285db1dc66e69869
Shakira Ndagire <ndagire.shakira@gmail.com>
2021-08-25 19:12:29 +0300

    [AC-9098] Fix linting issues

--- accelerator/migrations/0066_partnerlabel.py
+++ accelerator/migrations/0066_partnerlabel.py
@@ -15,12 +15,13 @@ class Migration(migrations.Migration):
             name='PartnerLabel',
             fields=[
                 ('id', models.AutoField(auto_created=True,
-                primary_key=True, serialize=False, verbose_name='ID')),
-                ('created_at', models.DateTimeField(auto_now_add=True, null=True)),
+                    primary_key=True, serialize=False, verbose_name='ID')),
+                ('created_at', models.DateTimeField(auto_now_add=True,
+                null=True)),
                 ('updated_at', models.DateTimeField(auto_now=True, null=True)),
                 ('label', models.CharField(max_length=255)),
                 ('partners', models.ManyToManyField(blank=True,
-                to=settings.ACCELERATOR_PARTNER_MODEL)),
+                    to=settings.ACCELERATOR_PARTNER_MODEL)),
             ],
             options={
                 'abstract': False,
