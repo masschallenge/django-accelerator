0895c35ffe6f34e22e3216e24ce1660a6a83a8d7
Jon Kiparsky <jon@kiparsky.net>
2021-08-25 14:28:02 -0400

    [AC-9103] Add model and factory

---
+++ accelerator/models/partner_startup_interest.py
@@ -0,0 +1,13 @@
+# MIT License
+# Copyright (c) 2017 MassChallenge, Inc.
+
+import swapper
+
+from accelerator_abstract.models import BasePartnerStartupInterest
+
+
+class PartnerStartupInterest(BasePartnerStartupInterest):
+    class Meta(BasePartnerStartupInterest.Meta):
+        swappable = swapper.swappable_setting(
+            BasePartnerStartupInterest.Meta.app_label, "PartnerStartupInterest")
+
---
+++ accelerator_abstract/models/base_partner_startup_interest.py
@@ -0,0 +1,27 @@
+from swapper import get_model_name
+
+from django.db.models import (
+    CASCADE,
+    ForeignKey,
+    TextField,
+)
+
+from .accelerator_model import AcceleratorModel
+
+class BasePartnerStartupInterest(AcceleratorModel):
+    partner = ForeignKey(
+        get_model_name(AcceleratorModel.Meta.app_label,
+                       "Partner"),
+        on_delete=CASCADE)
+    judging_round = ForeignKey(
+        get_model_name(AcceleratorModel.Meta.app_label,
+                       "JudgingRound"),
+        on_delete=CASCADE)
+    startup = ForeignKey(
+        get_model_name(AcceleratorModel.Meta.app_label,
+                       "Startup"),
+        on_delete=CASCADE)
+
+    class Meta(AcceleratorModel.Meta):
+        abstract = True
+        db_table = 'accelerator_partnerstartupinterest'
