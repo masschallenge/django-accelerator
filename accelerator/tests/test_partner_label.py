ec7e37caae7dbd43622172f25eed78c45a7995dd
Shakira Ndagire <ndagire.shakira@gmail.com>
2021-08-25 18:42:16 +0300

    [AC-9098] Fix the class name

--- accelerator/tests/test_partner_label.py
+++ accelerator/tests/test_partner_label.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from accelerator.tests.factories import PartnerLabelFactory
 
 
-class TestStartupLabel(TestCase):
+class TestPartnerLabel(TestCase):
     def test_str(self):
         label = PartnerLabelFactory()
         assert label.label in str(label)
