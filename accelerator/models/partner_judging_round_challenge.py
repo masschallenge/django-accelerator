2db7b350a7fe1cbe1d814e9c4516094fa90f8370
Jon Kiparsky <jon@kiparsky.net>
2021-08-24 23:30:50 -0400

    [AC-9100] Add new model and factory

---
+++ accelerator/models/partner_judging_round_challenge.py
@@ -0,0 +1,13 @@
+# MIT License
+# Copyright (c) 2017 MassChallenge, Inc.
+
+import swapper
+
+from accelerator_abstract.models import BasePartnerJudgingRoundChallenge
+
+
+class PartnerJudgingRoundChallenge(BasePartnerJudgingRoundChallenge):
+    class Meta(BasePartnerJudgingRoundChallenge.Meta):
+        swappable = swapper.swappable_setting(
+            BasePartnerJudgingRoundChallenge.Meta.app_label, "PartnerJudgingRoundChallenge")
+
