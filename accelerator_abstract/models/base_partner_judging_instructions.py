458974d7b60120bf105b3776f5fbe576806d61b4
Amos Welike <amoswelike@gmail.com>
2021-08-26 20:10:04 +0300

    [AC-9102] implement feedback on PR

--- accelerator_abstract/models/base_partner_judging_instructions.py
+++ accelerator_abstract/models/base_partner_judging_instructions.py
@@ -14,7 +14,6 @@ class BasePartnerJudgingInstructions(AcceleratorModel):
                                "JudgingRound"),
         on_delete=models.CASCADE)
     instructions = models.TextField(
-        max_length=1000,
         blank=False,
         help_text='Partner Judging instructions for judging rounds')
 
@@ -22,7 +21,3 @@ class BasePartnerJudgingInstructions(AcceleratorModel):
         abstract = True
         verbose_name_plural = "instructions from a partner"
         unique_together = ('partner', 'judging_round')
-
-    def __str__(self):
-        return "%s added instruction %s for %s " % (
-            self.partner, self.instructions, self.judging_round)
