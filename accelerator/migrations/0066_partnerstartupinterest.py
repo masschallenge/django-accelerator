7bf3743187b965b9a4ee170334d04f1138048f7b
Jon Kiparsky <jon@kiparsky.net>
2021-08-26 11:23:09 -0400

    [AC-9103] update migration

--- accelerator/migrations/0066_partnerstartupinterest.py
+++ accelerator/migrations/0066_partnerstartupinterest.py
@@ -8,7 +8,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accelerator', '0065_organization_note'),
+        ('accelerator', '0066_partnerlabel'),
     ]
 
     operations = [
