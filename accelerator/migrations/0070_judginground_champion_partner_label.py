59c3e472c16252e1993f71f4f052184092fc6d33
Amos Welike <amoswelike@gmail.com>
2021-08-31 17:41:12 +0300

    [AC-9104] correct linting

--- accelerator/migrations/0070_judginground_champion_partner_label.py
+++ accelerator/migrations/0070_judginground_champion_partner_label.py
@@ -15,10 +15,11 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='judginground',
             name='champion_partner_label',
-            field=models.ForeignKey(blank=True,
-            help_text='Partner Label',
-            null=True,
-            on_delete=django.db.models.deletion.CASCADE,
-            to=settings.ACCELERATOR_PARTNERLABEL_MODEL),
+            field=models.ForeignKey(
+                blank=True,
+                help_text='Partner Label',
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                to=settings.ACCELERATOR_PARTNERLABEL_MODEL),
         ),
     ]
