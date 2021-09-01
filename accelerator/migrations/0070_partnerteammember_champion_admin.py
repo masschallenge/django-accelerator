0586057b5df1084e847493e6c5f8ffc641c68c54
Jon Kiparsky <jon@kiparsky.net>
2021-09-01 09:57:53 -0400

    [AC-9107] update migration

--- accelerator/migrations/0070_partnerteammember_champion_admin.py
+++ accelerator/migrations/0070_partnerteammember_champion_admin.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accelerator', '0066_partnerlabel'),
+        ('accelerator', '0069_partnerstartupinterest')
     ]
 
     operations = [
