72b862faa2a81f8ac5775fd7d9a2faad49c0c428
Shakira Ndagire <ndagire.shakira@gmail.com>
2021-11-09 18:23:59 +0300

    [AC-9046] Fix linting issues

--- accelerator/migrations/0074_update_url_to_community.py
+++ accelerator/migrations/0074_update_url_to_community.py
@@ -10,7 +10,8 @@ def update_url_to_community(apps, schema_editor):
     community_url = "/community"
     SiteRedirectPage = apps.get_model('accelerator', 'SiteRedirectPage')
     SiteRedirectPage.objects.filter(
-      Q(new_url=people_url) | Q(new_url=mentor_url)).update(new_url=community_url)
+      Q(new_url=people_url)| Q(new_url=mentor_url)
+      ).update(new_url=community_url)
 
 
 class Migration(migrations.Migration):
