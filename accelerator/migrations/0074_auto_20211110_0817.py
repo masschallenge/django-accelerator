cbf9b4204c6adf9a70bc2520e5e9e070185dc75f
Amos Welike <amoswelike@gmail.com>
2021-11-10 16:25:53 +0300

    [AC-9123] correct lint

--- accelerator/migrations/0074_auto_20211110_0817.py
+++ accelerator/migrations/0074_auto_20211110_0817.py
@@ -2,19 +2,23 @@
 
 from django.db import migrations
 
+
 def remove_finalist_role_from_staff(apps, schema_editor):
-    staff_ids= User.objects.filter(
+    User = apps.get_model("accelerator", "User")
+    ProgramRoleGrant = apps.get_model("accelerator", "ProgramRoleGrant")
+    staff_ids = User.objects.filter(
         programrolegrant__program_role__user_role__name='Finalist',
         is_staff=True).values_list('id', flat=True)
 
-    staff_finalist_role=ProgramRoleGrant.objects.filter(
+    staff_finalist_role = ProgramRoleGrant.objects.filter(
                 program_role__user_role__name='Finalist',
                 person_id__in=staff_ids).values_list(
                     'id', flat=True)
 
     for role in staff_finalist_role:
         role.delete()
-        
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
