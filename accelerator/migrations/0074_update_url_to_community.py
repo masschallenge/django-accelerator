08a95f7793d496d36cc0a753694c2975b2f30c68
Shakira Ndagire <ndagire.shakira@gmail.com>
2021-11-10 16:47:48 +0300

    [AC-9046] Add another constraint for the url

--- accelerator/migrations/0074_update_url_to_community.py
+++ accelerator/migrations/0074_update_url_to_community.py
@@ -5,13 +5,21 @@ from django.db.models.query_utils import Q
 
 
 def update_url_to_community(apps, schema_editor):
-    people_url = "/people"
+    people_url = ["/people", "/people"]
     mentor_url = "/directory"
+    mentor_refinement_url = "/directory/?refinementList%5Bhome_program_family%5D%5B0%5D=Israel"
     community_url = "/community"
+    community_refinement_url = "/community/?refinementList%5Bprogram_family_names%5D%5B0%5D=Israel"
+
     SiteRedirectPage = apps.get_model('accelerator', 'SiteRedirectPage')
     SiteRedirectPage.objects.filter(
-      Q(new_url=people_url) | Q(new_url=mentor_url)
+      Q(new_url__in=people_url) | Q(new_url=mentor_url)
       ).update(new_url=community_url)
+
+    if mentor_refinement_url:
+      SiteRedirectPage.objects.filter(
+      mentor_refinement_url
+      ).update(community_refinement_url)
 
 
 class Migration(migrations.Migration):
