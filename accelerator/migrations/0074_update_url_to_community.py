80cf78d9753643e7a9ef9aac96807b79e1020133
Shakira Ndagire <ndagire.shakira@gmail.com>
2021-11-12 15:52:54 +0300

    [AC-9046] Fix linting issues

--- accelerator/migrations/0074_update_url_to_community.py
+++ accelerator/migrations/0074_update_url_to_community.py
@@ -9,7 +9,7 @@ def update_url_to_community(apps, schema_editor):
     mentor_url = "/directory"
     community_url = "/community"
     mentor_refinement_url = ("/directory/?refinementList%5B"
-                            "home_program_family%5D%5B0%5D=Israel")
+                             "home_program_family%5D%5B0%5D=Israel")
     community_refinement_url = ("/community/?refinementList%5B"
                                 "program_family_names%5D%5B0%5D=Israel")
 
