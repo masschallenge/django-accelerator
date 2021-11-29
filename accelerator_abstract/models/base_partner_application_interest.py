bb21c1210c559ad9e2748bc275d23960ba8c473f
Shakira Ndagire <ndagire.shakira@gmail.com>
2021-11-29 21:35:59 +0300

    [AC-9265] Make the fields unique and show the __repr__ text

--- accelerator_abstract/models/base_partner_application_interest.py
+++ accelerator_abstract/models/base_partner_application_interest.py
@@ -13,17 +13,27 @@ class BasePartnerApplicationInterest(AcceleratorModel):
     partner = ForeignKey(
         get_model_name(AcceleratorModel.Meta.app_label,
                        "Partner"),
+        unique=True,
         on_delete=CASCADE)
     judging_round = ForeignKey(
         get_model_name(AcceleratorModel.Meta.app_label,
                        "JudgingRound"),
+        unique=True,
         on_delete=CASCADE)
     application = ForeignKey(
         get_model_name(AcceleratorModel.Meta.app_label,
                        "Application"),
+        unique=True,
         on_delete=CASCADE)
     advance_to_next_round = BooleanField(default=False)
 
     class Meta(AcceleratorModel.Meta):
         abstract = True
         db_table = 'accelerator_partnerapplicationinterest'
+
+    def __repr__(self):
+        return "PartnerApplicationInterest {} - {} - {}".format(
+            self.partner.organization.name, 
+            self.application.startup.organization.name,
+            self.judging_round.name
+            )
