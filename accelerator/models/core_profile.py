f25e5bc4acd47338383d1e3b076308cb27bc47d5
kenware <ejykken@gmail.com>
2021-12-02 11:43:58 +0100

    [AC-6538]: Add a different url

--- accelerator/models/core_profile.py
+++ accelerator/models/core_profile.py
@@ -103,8 +103,7 @@ class CoreProfile(BaseCoreProfile, PolymorphicModel):
     @property
     def mentor_profile_url(self):
         if self.is_mentor():
-            return reverse(
-                'people_directory_redirect_view') + f"people/{self.user.id}"
+            return reverse('community_profile', kwargs={'user_id': self.user.id})
 
     @property
     def active_round(self):
