1f5a37aa8010f47af5cc1d07bc4f702cc7bba45f
Shakira Ndagire <ndagire.shakira@gmail.com>
2021-12-07 15:18:15 +0300

    [AC-9265] Add unique together constartint on partnerapplicationinterest

--- accelerator_abstract/models/base_partner_application_interest.py
+++ accelerator_abstract/models/base_partner_application_interest.py
@@ -13,23 +13,22 @@ class BasePartnerApplicationInterest(AcceleratorModel):
     partner = ForeignKey(
         get_model_name(AcceleratorModel.Meta.app_label,
                        "Partner"),
-        unique=True,
         on_delete=CASCADE)
     judging_round = ForeignKey(
         get_model_name(AcceleratorModel.Meta.app_label,
                        "JudgingRound"),
-        unique=True,
         on_delete=CASCADE)
     application = ForeignKey(
         get_model_name(AcceleratorModel.Meta.app_label,
                        "Application"),
-        unique=True,
         on_delete=CASCADE)
     advance_to_next_round = BooleanField(default=False)
 
     class Meta(AcceleratorModel.Meta):
         abstract = True
         db_table = 'accelerator_partnerapplicationinterest'
+        unique_together = (('partner', 'judging_round',
+                            'application'),)
 
     def __repr__(self):
         return "PartnerApplicationInterest {} - {} - {}".format(
