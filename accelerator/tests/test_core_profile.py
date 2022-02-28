ffca1973c4838f10bb7070a4e4cc34f527ba45a6
Jon Kiparsky <jon.kiparsky@gmail.com>
2022-02-28 16:33:27 -0500

    [AC-9402] fix typo

--- accelerator/tests/test_core_profile.py
+++ accelerator/tests/test_core_profile.py
@@ -368,15 +368,15 @@ class TestCoreProfile(TestCase):
         'activate_unified_profile_views', False))        
     def test_users_with_entrepreneur_interest_get_startups_landing_page(self):
         profile = CoreProfileModelFactory(entrepreneur_interest=True)
-        expected = 'profile'
+        expected = 'applicant_homepage'
         self.assertEqual(profile.check_landing_page(), expected)
-
+        
     @patch(flag_smith_has_feature, fake_flag_smith_response(
         'activate_unified_profile_views', True))
     def test_users_with_entrepreneur_interest_get_profile_as_landing_page(self):
         profile = CoreProfileModelFactory(entrepreneur_interest=True)
         expected = 'profile'
-        self.assertEqual(profile.check_landing_p[Oage(), expected)        
+        self.assertEqual(profile.check_landing_page(), expected)    
 
     def was_mentor_in_last_12_months(self):
         profile = ExpertProfileFactory()
