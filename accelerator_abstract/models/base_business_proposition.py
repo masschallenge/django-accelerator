98e9a97fdbb3d3b2c0f4a3f7f7e6d745628c32de
kenware <ejykken@gmail.com>
2022-03-16 18:37:55 +0100

    [AC-9447]: Add help text to business proposition view

--- accelerator_abstract/models/base_business_proposition.py
+++ accelerator_abstract/models/base_business_proposition.py
@@ -5,6 +5,41 @@ from accelerator_abstract.models.accelerator_model import AcceleratorModel
 
 EXCLUDED_FIELDS = ['id', 'created_at', 'updated_at', 'startup']
 
+PAIN_POINT_HELP_TEXT = ('Please describe what '
+                        'problem you are trying to solve?')
+SOLUTION_HELP_TEXT = ('What is innovative about '
+                      'your solution, technology, business model etc?')
+IMPACT_HELP_TEXT = ('Define the 1 year and 5 '
+                   'year impact that you hope to accomplish')
+MARKET_HELP_TEXT = ('How would you define your potential market '
+                    'and what is the total addressable market size?')
+VALUE_PROPOSITION_HELP_TEXT = ('What will be your messaging to users & '
+                               'customers and how do you plan to spread'
+                               ' that message?')
+SALES_HELP_TEXT = ('How will you reach your customers? via which '
+                   'channels will you likely reach your customers/users?')
+COMPETITORS_HELP_TEXT = ('Which organizations compete with'
+                         ' your current value offering and who might '
+                         'do so in the future?')
+PRODUCT_COMPLEMENT_HELP_TEXT = ('Which organizations compete with your '
+                                'current value offering and who might'
+                                ' do so in the future?')
+PRIMARY_ADVANTAGE_HELP_TEXT = ('What are the primary advantages'
+                               ' over competitors')
+DRIVER_HELP_TEXT = ('What are the key drivers of '
+                    'business economics (price points, margins, etc)?')
+INTELLECTUAL_PROPERTY_HELP_TEXT = ('What IP (Intellectual Property) exist'
+                                   ' for your business or in your industry?')
+REGULATORY_HELP_TEXT = ('What regulatory requirements exist '
+                        'for your business or in your industry?')
+TEAM_SUMMARY_HELP_TEXT = ('Please share some background information '
+                          'on your team members and tell us what makes'
+                          ' your team special.')
+INVESTORS_HELP_TEXT = ('Please tell us about current or '
+                       'anticipated advisors and investors.')
+VALIDATION_HELP_TEXT = ('What traction have you made to '
+                        'date with market validation?')
+
 
 class BaseBusinessProposition(AcceleratorModel):
     startup = models.ForeignKey(
@@ -12,9 +47,11 @@ class BaseBusinessProposition(AcceleratorModel):
         on_delete=models.CASCADE)
     pain_point = models.TextField(blank=True,
                                   null=True,
+                                  help_text=PAIN_POINT_HELP_TEXT,
                                   verbose_name="Customer Pain Point")
     solution = models.TextField(blank=True,
                                 null=True,
+                                help_text=SOLUTION_HELP_TEXT,
                                 verbose_name='Solution')
     impact = models.TextField(blank=True,
                               null=True,
@@ -22,50 +59,62 @@ class BaseBusinessProposition(AcceleratorModel):
     market = models.TextField(
         blank=True,
         null=True,
+        help_text=MARKET_HELP_TEXT,
         verbose_name='Potential Market / Addressable Size')
     value_proposition = models.TextField(
         blank=True,
         null=True,
+        help_text=VALUE_PROPOSITION_HELP_TEXT,
         verbose_name='Value Proposition / Marketing Message')
     sales = models.TextField(
         blank=True,
         null=True,
+        help_text=SALES_HELP_TEXT,
         verbose_name='Sales and Distribution / Channels')
     competitors = models.TextField(
         blank=True,
         null=True,
+        help_text=COMPETITORS_HELP_TEXT,
         verbose_name='Current and Future Competitors')
     product_complements = models.TextField(
         blank=True,
         null=True,
+        help_text=PRODUCT_COMPLEMENT_HELP_TEXT,
         verbose_name='Product Complements / Value Chain Partners')
     primary_advantages = models.TextField(
         blank=True,
         null=True,
+        help_text=PRIMARY_ADVANTAGE_HELP_TEXT,
         verbose_name='Primary Advantages vs Competitors')
     drivers = models.TextField(
         blank=True,
         null=True,
+         help_text=DRIVER_HELP_TEXT,
         verbose_name='Key Drivers of Business Economics')
     intellectual_property = models.TextField(
         blank=True,
         null=True,
+        help_text=INTELLECTUAL_PROPERTY_HELP_TEXT,
         verbose_name='Intellectual Property')
     regulation = models.TextField(
         blank=True,
         null=True,
+        help_text=REGULATORY_HELP_TEXT,
         verbose_name='Regulatory Requirements')
     team_summary = models.TextField(
         blank=True,
         null=True,
+        help_text=TEAM_SUMMARY_HELP_TEXT,
         verbose_name='Team (Backgrounds, advantages)')
     investors = models.TextField(
         blank=True,
         null=True,
+        help_text=INVESTORS_HELP_TEXT,
         verbose_name='Current or anticipated advisors or investors')
     validation = models.TextField(
         blank=True,
         null=True,
+        help_text=VALIDATION_HELP_TEXT,
         verbose_name='Traction and Market Validation')
 
     class Meta(AcceleratorModel.Meta):
