4be238425ebb22e34203d0ed78ebf6615614a204
Ian Kigen <ian.gabe.ian@gmail.com>
2022-04-19 18:29:02 +0300

    [AC-9444] add migrations

--- accelerator/models/innovation_stage.py
+++ accelerator/models/innovation_stage.py
@@ -7,5 +7,6 @@ from accelerator_abstract.models.accelerator_model import AcceleratorModel
 class InnovationStage(AcceleratorModel):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    image = ImageField(upload_to='innovation_stage_images', blank=True, null=True)
+    image = ImageField(
+        upload_to='innovation_stage_images', blank=True, null=True)
     display_priority = models.PositiveIntegerField()
