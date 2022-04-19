8c6bc9b50ee4c60deb781a2a06c595c5ba326413
Ian Kigen <ian.gabe.ian@gmail.com>
2022-04-19 18:26:58 +0300

    [AC-9444] add innovation stage model

---
+++ accelerator/models/innovation_stage.py
@@ -0,0 +1,11 @@
+from django.db import models
+from sorl.thumbnail import ImageField
+
+from accelerator_abstract.models.accelerator_model import AcceleratorModel
+
+
+class InnovationStage(AcceleratorModel):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    image = ImageField(upload_to='innovation_stage_images', blank=True, null=True)
+    display_priority = models.PositiveIntegerField()
