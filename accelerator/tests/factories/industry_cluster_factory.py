fb42c2542c1de662adce4fe8d506e9200d0cb836
Amos Welike <amoswelike@gmail.com>
2022-04-20 19:18:52 +0300

    [AC-9445] edit name and description factory values

--- accelerator/tests/factories/industry_cluster_factory.py
+++ accelerator/tests/factories/industry_cluster_factory.py
@@ -9,8 +9,8 @@ class IndustryClusterFactory(DjangoModelFactory):
     class Meta:
         model = IndustryCluster
 
-    name = Sequence(lambda n: "Climate Cluster {0}".format(n))
+    name = Sequence(lambda n: "Industry Cluster {0}".format(n))
     description = Sequence(
-        lambda n: "Program interested in Climate {0}".format(n))
+        lambda n: "Industry Cluster description {0}".format(n))
     display_priority = Sequence(lambda n: n)
     image = None
