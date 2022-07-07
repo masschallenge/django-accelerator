6cdd32f5037fcfda2a5dd532dba7b7314522f654
kenware <ejykken@gmail.com>
2022-07-07 15:55:36 +0100

    [AC-9556]: Remove bucket list report

--- accelerator/migrations/0110_remove_bucket_list_program_role_20220707_1001.py
+++ accelerator/migrations/0110_remove_bucket_list_program_role_20220707_1001.py
@@ -4,7 +4,16 @@ from django.db import migrations
 def remove_bucket_list_program_roles(apps, schema_editor):
     BucketState = apps.get_model('accelerator', 'BucketState')
     ProgramRole = apps.get_model('accelerator', 'ProgramRole')
-    program_role_ids = BucketState.objects.values_list('program_role_id', flat=True)
+    ProgramRoleGrant = apps.get_model('accelerator', 'ProgramRoleGrant')
+    NodePublishedFor = apps.get_model('accelerator', 'NodePublishedFor')
+
+    program_role_ids = BucketState.objects.values_list('program_role_id',
+                                                        flat=True)
+    NodePublishedFor.objects.filter(
+        published_for_id__in=program_role_ids).delete()
+    ProgramRoleGrant.objects.filter(
+        program_role_id__in=program_role_ids).delete()
+    BucketState.objects.all().delete()
     ProgramRole.objects.filter(pk__in=program_role_ids).delete()
 
 
