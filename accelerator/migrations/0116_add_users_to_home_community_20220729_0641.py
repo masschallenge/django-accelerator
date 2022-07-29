b6b37d75cd04e627aae8216aea4b7d13726eb8a4
kenware <ejykken@gmail.com>
2022-07-29 16:49:38 +0100

    [AC-9669]: Fix migration conflict

--- accelerator/migrations/0116_add_users_to_home_community_20220729_0641.py
+++ accelerator/migrations/0116_add_users_to_home_community_20220729_0641.py
@@ -26,7 +26,7 @@ def add_home_community_to_users(apps, schema_editor):
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accelerator', '0115_add_ecosystem_model'),
+        ('accelerator', '0116_add_program_menu_sidenav'),
     ]
 
     operations = [
